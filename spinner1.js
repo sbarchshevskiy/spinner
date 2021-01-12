5f00a016c6142b863ce98afeac723a25f37be174
sergey <sbarchshevskiy@gmail.com>
2021-01-12 19:24:30 +0000

    spinner

---
+++ spinner1.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
